6b18d60036a553bde4fbf6af1f6b9b489d09d191
nbindumadhavi <b18134337@gmail.com>
2024-09-30 17:24:20 +0530

    datatypes are added

---
+++ datatypes/fundamental_dt.py
@@ -0,0 +1,29 @@
+#int - to store integer numbers only
+a=10
+print(a, type(a))
+
+#float - to store decimal numbers/ floating point values
+b = 1.0
+print(b,type(b))
+
+#bool - to store logical values like true(1) or false(0)
+a= True
+b= False
+print(a+b)
+print(b-a)
+print(2 + a)
+print(True + 1)
+print( False + a)
+
+# complex - to store complex values
+a = 2+3j
+print(a,type(a))
+b = 2.3 + 4.5
+print(b,type(b))
+print(b.real)
+print(b.imag)
+print(2+6j+7+3j)
+print(2+5j.real)
+print(3+6j.imag)
+print((3+6j).imag)
+
