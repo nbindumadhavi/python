7c706d9325e78bc3048cd0227416ef38b5663553
nbindumadhavi <b18134337@gmail.com>
2024-09-30 15:37:04 +0530

    modified fail

--- variables.py
+++ variables.py
@@ -1,5 +1,21 @@
-# global variable
-x="awesome"
+## global variable
+# x="awesome"
+# def myfunc():
+#     print("python is " + x)
+# myfunc()
+
+#global key word
 def myfunc():
+    global x
+    x="fentastic"
     print("python is " + x)
 myfunc()
+
+# local and global variable
+x="awesome" #global variable
+def myfunc():
+    x="fentastic" #local variable
+    print("python is " + x)
+myfunc()
+print("python is " + x)
+
